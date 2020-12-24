bd21ec0f0fc0f74c85c01fb3850333d5194d9d71
Alex McConnell <alexmc3@gmail.com>
2020-12-23 21:32:10 -0500

    Moving Tricks creation logic into Tricks class.

--- src/deal.cr
+++ src/deal.cr
@@ -2,18 +2,14 @@ module CardGame
   class Deal
     @deck : Deck
     @hands : Hands
+    @tricks : Tricks
+
     getter :hands, :tricks
   
     def initialize
-      @tricks = Tricks.new
       @deck = Deck.create
       @hands = Hands.create(@deck)
-
-      new_trick
-    end
-
-    private def new_trick
-      @tricks << Trick.new
+      @tricks = Tricks.create
     end
   end
 end
--- src/tricks.cr
+++ src/tricks.cr
@@ -1,4 +1,9 @@
 module CardGame
   class Tricks < Array(Trick)
+    def self.create
+      tricks = Tricks.new
+      tricks << Trick.new
+      tricks
+    end
   end
 end
