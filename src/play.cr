75352e12151819b623387d3be194c052f7f179fe
Alex McConnell <alexmc3@gmail.com>
2020-12-27 21:14:46 -0500

    Refactoring Play logic.

--- src/play.cr
+++ src/play.cr
@@ -1,24 +1,25 @@
 module CardGame
   class Play
-    @game : Game
-    @hand_num : Int32
-    @card_str : String
+    @card : Card
+    @hand : Hand
+    @trick : Trick
 
     def initialize(game, hand_num, card_str)
-      @game = game
-      @hand_num = hand_num
-      @card_str = card_str
+      @card = Card.from_s(card_str)
+      @hand = game.hands[hand_num]
+      @trick = game.current_trick
     end
 
     def do
-      card = Card.from_s(@card_str)
-      hand = @game.hands[@hand_num]
-      if @game.current_trick.size > 0
-        led_suit = @game.current_trick.first.suit
-        return "Must follow suit." if led_suit != card.suit && hand.has_suit?(led_suit)
-      end
+      return "Must follow suit." unless suit_valid?
 
-      hand.transfer(card, @game.current_trick)
+      @hand.transfer(@card, @trick)
+    end
+
+    private def suit_valid?
+      return true if @trick.size == 0
+      suit = @trick.first.suit
+      suit == @card.suit || !@hand.has_suit?(suit)
     end
   end
 end
