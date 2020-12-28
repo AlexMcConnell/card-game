9b69984ecba492ad9168db656a639c47a1d563ba
Alex McConnell <alexmc3@gmail.com>
2020-12-27 20:13:25 -0500

    Moving logic for a play into a Play class.

--- src/game.cr
+++ src/game.cr
@@ -17,16 +17,7 @@ module CardGame
     end
 
     def play(hand_num, card_str)
-      card = Card.from_s(card_str)
-      hand = hands[hand_num]
-      if current_trick.size > 0
-        led_suit = current_trick.first.suit
-        return "Must follow suit." if led_suit != card.suit && hand.has_suit?(led_suit)
-      end
-
-      found_card = hand.delete(card)
-      raise MissingCardError.new unless found_card
-      current_trick << found_card
+      Play.new(self, hand_num, card_str).do
     end
   end
 end
---
+++ src/play.cr
@@ -0,0 +1,26 @@
+module CardGame
+  class Play
+    @game : Game
+    @hand_num : Int32
+    @card_str : String
+
+    def initialize(game, hand_num, card_str)
+      @game = game
+      @hand_num = hand_num
+      @card_str = card_str
+    end
+
+    def do
+      card = Card.from_s(@card_str)
+      hand = @game.hands[@hand_num]
+      if @game.current_trick.size > 0
+        led_suit = @game.current_trick.first.suit
+        return "Must follow suit." if led_suit != card.suit && hand.has_suit?(led_suit)
+      end
+
+      found_card = hand.delete(card)
+      raise MissingCardError.new unless found_card
+      @game.current_trick << found_card
+    end
+  end
+end
